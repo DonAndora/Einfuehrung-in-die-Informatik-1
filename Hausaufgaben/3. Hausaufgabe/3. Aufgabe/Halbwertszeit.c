df59cbfe5844cde0815cfb8b1b0bfbdc702bf349
Andora <andora.macbook@gmail.com>
2022-11-08 01:17:59 +0100

    Main

--- Hausaufgaben/3. Hausaufgabe/3. Aufgabe/Halbwertszeit.c
+++ Hausaufgaben/3. Hausaufgabe/3. Aufgabe/Halbwertszeit.c
@@ -2,5 +2,17 @@
 
 int main (void)
 {
-    
+    float wirkstoffgehalt = 100; //Ausgangswert in Prozent
+    int monate = 0; //Zähler für Monate
+
+    while (wirkstoffgehalt >= 50) //Zählerfunktion, solange der Wirkstoffgehalt größer gleich 50 ist. Danach wird die Schleife verlassen.
+    {
+        printf("Monat: %d Wirkstoffgehalt: %f\n", monate, wirkstoffgehalt);
+        wirkstoffgehalt = wirkstoffgehalt * 0.96;
+        monate++;
+    }
+    printf("Monat: %d Wirkstoffgehalt: %f ABGELAUFEN\n", monate, wirkstoffgehalt); //Ausgabe für den letzten Monat, da der Wirkstoffgehalt kleiner als 50 ist.
+
+
+    return 0;
 }
