f9c9bca358805c86ff073c2937234a839677ffa1
Andora <andora.macbook@gmail.com>
2022-11-08 01:31:11 +0100

    Main

--- Hausaufgaben/3. Hausaufgabe/4. Aufgabe/Epsilon.c
+++ Hausaufgaben/3. Hausaufgabe/4. Aufgabe/Epsilon.c
@@ -2,12 +2,12 @@
 
 int main (void)
 {
-    float eps = 1.0;
-    while (1.0 + eps != 1.0)
+    float eps = 1.0; //Variable eps als float deklarieren und initialisieren mit dem Wert 1.0
+    while (1.0 + eps != 1.0) //Solange 1.0 + eps ungleich 1.0 ist, wird eps halbiert.
     {
         eps = eps / 2.0;
     }
-    printf("Eps: %f\n", eps);
+    printf("Eps: %f\n", eps); //Sobald 1.0 + eps gleich 1.0 ist, wird eps ausgegeben.
 
     return 0;
 }
