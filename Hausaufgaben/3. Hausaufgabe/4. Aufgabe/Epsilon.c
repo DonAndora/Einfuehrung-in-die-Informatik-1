d89958ef66db2e9a71e8454e7073054d0a1ba240
Andora <andora.macbook@gmail.com>
2022-11-09 14:05:19 +0100

    Update Epsilon.c

--- Hausaufgaben/3. Hausaufgabe/4. Aufgabe/Epsilon.c
+++ Hausaufgaben/3. Hausaufgabe/4. Aufgabe/Epsilon.c
@@ -2,7 +2,7 @@
 
 int main (void)
 {
-    float eps = 1.0; //Variable eps als float deklarieren und initialisieren mit dem Wert 1.0
+    float eps = 1.0f; //Variable eps als float deklarieren und initialisieren mit dem Wert 1.0
     while (1.0 + eps != 1.0) //Solange 1.0 + eps ungleich 1.0 ist, wird eps halbiert.
     {
         eps = eps / 2.0;
