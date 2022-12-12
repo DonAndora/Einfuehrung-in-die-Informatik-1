b4c931e30097d98b7d3a9498e9b2c498287fa7de
Andora <andora.macbook@gmail.com>
2022-12-12 03:18:09 +0100

    Updated

--- Hausaufgaben/7. Hausaufgabe/3. Aufgabe/mypoint.hpp
+++ Hausaufgaben/7. Hausaufgabe/3. Aufgabe/mypoint.hpp
@@ -8,11 +8,11 @@ private:
   int x;
   int y;
   int z;
+
 public:
   MyPoint(int x, int y, int z);
 
   // Ihr Code hier
-
   double dist(MyPoint *q);
 
   void show();
--- Hausaufgaben/7. Hausaufgabe/3. Aufgabe/testpoint.cpp
+++ Hausaufgaben/7. Hausaufgabe/3. Aufgabe/testpoint.cpp
@@ -8,9 +8,10 @@ int main()
 {
   MyPoint* p = new MyPoint(1, -3, 2);
   MyPoint* q = new MyPoint(4, 8, 2);
-        
+     
   p->show();
   q->show();
+  
   std::cout << "Abstand: " << p->dist(q) << std::endl;
 
   delete p;
